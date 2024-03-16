a762a66ffec9ec35c0726f0bbb8fb5c9fab38a48
Gopal S <78960894+MaDGaMeR142@users.noreply.github.com>
2024-03-16 17:00:43 +0530

    V5.6

--- lib/pages/admin_query.dart
+++ lib/pages/admin_query.dart
@@ -1,7 +1,27 @@
+import 'package:cwrdm/database/queries.dart';
 import 'package:flutter/material.dart';
 
-class AdminQueryPage extends StatelessWidget {
+class AdminQueryPage extends StatefulWidget {
+  const AdminQueryPage({super.key});
+
   @override
+  State<AdminQueryPage> createState() => _AdminQueryPageState();
+}
+
+class _AdminQueryPageState extends State<AdminQueryPage> {
+  List<Query> queries = [];
+
+  void init() async {
+    queries = await getAdminQueries();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
